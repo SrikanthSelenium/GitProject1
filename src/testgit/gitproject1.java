9548d638ca404597e19fa3369efe01d5d530cd1a
= <=>
2017-11-03 23:27:25 +0530

    all files are added

--- src/testgit/gitproject1.java
+++ src/testgit/gitproject1.java
@@ -1,18 +1,18 @@
 package testgit;
 
+import org.testng.annotations.Test;
+
 public class gitproject1 {
 	
 	static int i ;
 	static int j;
 	
-	public static void main(String[] args) {
+	@Test
+	public void prgm(){
 		System.out.println(i+" "+j);
 		System.out.println("added to git hub");
 		System.out.println("added to eclipse");
-		
 		System.out.println("b1 line added to eclipse");
-		
-
 		System.out.println("master line added to eclipse");
 
 	}
