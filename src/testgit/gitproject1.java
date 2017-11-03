61adb5627da7c5dd633747813dd7e4b3528277e0
= <=>
2017-11-03 23:10:01 +0530

    b1 line added

--- src/testgit/gitproject1.java
+++ src/testgit/gitproject1.java
@@ -9,6 +9,8 @@ public class gitproject1 {
 		System.out.println(i+" "+j);
 		System.out.println("added to git hub");
 		System.out.println("added to eclipse");
+		
+		System.out.println("b1 line added to eclipse");
 	}
 
 }
